5f758ddae928dd4f6830da2df6ff2004a71fcdee
Mayank_Budaniya <155288695+MayankK806926@users.noreply.github.com>
2024-11-15 11:40:44 +0530

    Create K_th_number_having_X_as_digital_root.cpp

---
+++ K_th_number_having_X_as_digital_root.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll k,x;
+        cin>>k>>x;
+        ll res=(9*(k-1))+x;
+        cout<<res<<endl;
+    }
+}
