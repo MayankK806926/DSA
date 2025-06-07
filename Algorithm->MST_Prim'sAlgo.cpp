d77b758e533947d5a73998456b8c0de6ab807688
Mayank_Budaniya <155288695+MayankK806926@users.noreply.github.com>
2025-06-07 15:22:31 +0530

    Create Algorithm->MST_Prim'sAlgo.cpp

---
+++ Algorithm->MST_Prim'sAlgo.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        vector<vector<int>> ans;
+        int mstsum=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>> pq;
+        pq.push({0,{0,-1}});
+        vector<int> vis(V);
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int node=pq.top().second.first;
+            int par=pq.top().second.second;
+            pq.pop();
+            if(vis[node]) continue;
+            else{
+                ans.push_back({node,par});
+                vis[node]=1;
+                mstsum+=wt;
+            }
+            
+            for(auto& it:adj[node]){
+                if(!vis[it[0]]){
+                    pq.push({it[1],{it[0],node}});
+                }
+            }
+        }
+        
+        return mstsum;
+    }
+};
