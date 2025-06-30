0cf79adb974e6feb9a26ef469d3b08a3f37df76e
Mayank_Kumar <155288695+MayankK806926@users.noreply.github.com>
2025-06-30 18:18:08 +0530

    Create StackUse.cpp

---
+++ StackUse.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> NSE(vector<int>& arr,int n){
+    stack<int> st;
+    vector<int> ans(n,-1);
+    for(int i=n-1;i>=0;--i){
+        while(!st.empty() && arr[i] < st.top()) st.pop();
+        if(!st.empty()) ans[i]=st.top();
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+vector<int> NGE(vector<int>& arr,int n){
+    stack<int> st;
+    vector<int> ans(n,-1);
+    for(int i=n-1;i>=0;--i){
+        while(!st.empty() && arr[i] >= st.top()) st.pop();
+        if(!st.empty()) ans[i]=st.top();
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+
+vector<int> PSE(vector<int>& arr,int n){
+    stack<int> st;
+    vector<int> ans(n,-1);
+    for(int i=0;i<n;++i){
+        while(!st.empty() && arr[i] < st.top()) st.pop();
+        if(!st.empty()) ans[i]=st.top();
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+vector<int> PGE(vector<int>& arr,int n){
+    stack<int> st;
+    vector<int> ans(n,-1);
+    for(int i=0;i<n;++i){
+        while(!st.empty() && arr[i] >= st.top()) st.pop();
+        if(!st.empty()) ans[i]=st.top();
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+
+int main(){
+    int n;cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;++i) cin>>arr[i];
+    vector<int> nse=NSE(arr,n);
+
+    for(auto& it:nse) cout<<it<<" ";
+    cout<<endl;
+    vector<int> pse=PSE(arr,n);
+
+    for(auto& it:pse) cout<<it<<" ";
+    cout<<endl;
+
+    vector<int> nge=NGE(arr,n);
+
+    for(auto& it:nge) cout<<it<<" ";
+    cout<<endl;
+    vector<int> pge=PGE(arr,n);
+
+    for(auto& it:pge) cout<<it<<" ";
+    cout<<endl;
+}
